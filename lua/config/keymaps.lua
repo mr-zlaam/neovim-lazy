d9ebaf1ac0e37c926b4c1482a60b4f0d42e9a11b
mr-zlaam <zlaam.dev@gmail.com>
2024-09-18 01:32:08 +0500

    add new configurations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 vim.api.nvim_set_keymap("n", "<C-p>", ":bprevious<CR>", { noremap = true, silent = true })
 
 -- Move to the next buffer with Ctrl+N
-vim.api.nvim_set_keymap("n", "<C-n>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-o>", ":bnext<CR>", { noremap = true, silent = true })
 -- Delete the word behind the cursor without copying to clipboard in Insert mode
 vim.api.nvim_set_keymap("i", "<A-BS>", '<C-o>"_db', { noremap = true, silent = true })
 
@@ -34,3 +34,4 @@ vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
 -- In your init.lua or another configuration file
 -- Define the delete operation in visual mode to not use the clipboard
 vim.api.nvim_set_keymap("x", "d", '"_d', { noremap = true, silent = true })
+-- Key mappings for vim-visual-multi
