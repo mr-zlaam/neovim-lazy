108e5f94deb8e80072bad178413d881b1c9e0671
zlaam <zlaam.dev@gmail.com>
2025-06-24 03:11:52 +0500

    feat: Sync with system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- ~/.config/nvim/lua/config/options.lua
+-- NOTE THAT IF YOU ARE ON WAYLAND YOU HAVE TO INSTALL WL CLIPBOARD FIRST
+vim.opt.clipboard = "unnamedplus"
